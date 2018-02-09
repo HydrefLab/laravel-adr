0f6b0ae6c3f75d0e179f5223b3b004163b838f05
Michał Golon <michal.golon@gmail.com>
2018-02-09 12:55:25 +0100

    Add action resolver

---
+++ src/Action/ActionResolver.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HydrefLab\Laravel\ADR\Action;
+
+use Illuminate\Support\Str;
+
+class ActionResolver
+{
+    /**
+     * @param string $namespace
+     * @param string $resource
+     * @param string $actionType
+     * @return string
+     */
+    public static function resolveClassName(string $namespace, string $resource, string $actionType): string
+    {
+        $resource = ('index' !== Str::lower($actionType)) ? Str::singular($resource) : Str::plural($resource);
+
+        $actionClassName = sprintf(
+            '%s\%s%sAction',
+            $namespace,
+            ucfirst(Str::lower($actionType)),
+            ucfirst(Str::lower($resource))
+        );
+
+        return trim($actionClassName, '\\');
+    }
+}
